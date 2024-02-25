ebfb0b716a7b039e2fb3e92fe02c42e7b983bfeb
Kris-4321 <kristianpanev@gmail.com>
2024-02-25 15:45:40 +0200

    3;45

--- UpdateDetector.go
+++ UpdateDetector.go
@@ -6,4 +6,13 @@ const (
 	dburl          = "mongodb://localhost:27017"
 )
 
-func main()
+type Mongotemplate struct {
+	id          string `json: "id"`
+	competitors string `json: "competitors"`
+	lastchecked string `json: "last_checked"`
+	lastchange  string `json: "last_change"`
+}
+
+func main() {
+
+}
